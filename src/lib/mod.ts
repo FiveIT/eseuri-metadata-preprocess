99622171b56cad2bb1360635e1fa5dd962f9b3e5
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-03-23 13:58:14 +0200

    Create lib module main file.

---
+++ src/lib/mod.ts
@@ -0,0 +1,5 @@
+export * from "./util.ts";
+export { default as fetchCounties } from "./counties.ts";
+export { default as sql } from "./sql.ts";
+export * as sqlutil from "./sql.ts";
+export * as log from "./log.ts";
