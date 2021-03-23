ee13e3e6f784f92f594ec8f80606c5b2928acee6
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-03-23 13:52:26 +0200

    Add dependencies

---
+++ src/deps.ts
@@ -0,0 +1,5 @@
+export { blue, green, red } from "https://deno.land/std@0.91.0/fmt/colors.ts";
+export { readerFromStreamReader } from "https://deno.land/std@0.91.0/io/streams.ts";
+export { readCSVObjects } from "https://deno.land/x/csv@v0.5.1/mod.ts";
+export * as path from "https://deno.land/std@0.91.0/path/mod.ts";
+export { debug } from "https://deno.land/x/debug@0.2.0/mod.ts";
