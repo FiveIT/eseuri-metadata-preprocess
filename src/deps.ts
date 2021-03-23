7be4415743355d246064dac69e473b5ce0b77bdb
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-03-23 22:30:31 +0200

    Add unstable fs dependency

--- src/deps.ts
+++ src/deps.ts
@@ -2,3 +2,4 @@ export { blue, green, red } from "https://deno.land/std@0.91.0/fmt/colors.ts";
 export { readerFromStreamReader } from "https://deno.land/std@0.91.0/io/streams.ts";
 export { readCSVObjects } from "https://deno.land/x/csv@v0.5.1/mod.ts";
 export * as path from "https://deno.land/std@0.91.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.91.0/fs/mod.ts";
