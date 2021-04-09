a488a5b88053cbbcfb8a49aa6c8a113f6b21ce66
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-09 18:59:10 +0300

    Add verbose logging for function

--- src/lib/meta.ts
+++ src/lib/meta.ts
@@ -1,11 +1,12 @@
-import { sqlutil } from "lib";
+import { log, sqlutil } from "lib";
 import { readCSVObjects } from "deps";
 
 export default async function* (
   reader: Deno.Reader,
 ): AsyncGenerator<sqlutil.Input> {
   for await (const row of readCSVObjects(reader)) {
-    yield Object.fromEntries(
+    log.verbose("Before:", row);
+    const obj = Object.fromEntries(
       Object.entries(row).map((
         [key, value],
       ) => [
@@ -13,5 +14,7 @@ export default async function* (
         key === "id" || key.endsWith("_id") ? parseInt(value) : value,
       ]),
     );
+    log.verbose("After:", obj);
+    yield obj;
   }
 }
