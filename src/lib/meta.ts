f5424a58e4aeaa8f3225e43aa1723cbc1839d0a5
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-09 16:22:47 +0300

    Convert IDs to integers

--- src/lib/meta.ts
+++ src/lib/meta.ts
@@ -5,6 +5,13 @@ export default async function* (
   reader: Deno.Reader,
 ): AsyncGenerator<sqlutil.Input> {
   for await (const row of readCSVObjects(reader)) {
-    yield row;
+    yield Object.fromEntries(
+      Object.entries(row).map((
+        [key, value],
+      ) => [
+        key,
+        key === "id" || key.endsWith("_id") ? parseInt(value) : value,
+      ]),
+    );
   }
 }
