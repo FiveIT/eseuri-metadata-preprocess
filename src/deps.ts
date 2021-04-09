fb5ec7a454661b93741dab56bb121a27b41590e1
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-09 12:23:12 +0300

    Bump dependencies version

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
-export { blue, green, red } from "https://deno.land/std@0.91.0/fmt/colors.ts";
-export { readerFromStreamReader } from "https://deno.land/std@0.91.0/io/streams.ts";
+export { blue, green, red } from "https://deno.land/std@0.92.0/fmt/colors.ts";
+export { readerFromStreamReader } from "https://deno.land/std@0.92.0/io/streams.ts";
 export { readCSVObjects } from "https://deno.land/x/csv@v0.5.1/mod.ts";
-export * as path from "https://deno.land/std@0.91.0/path/mod.ts";
-export * as fs from "https://deno.land/std@0.91.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.92.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.92.0/fs/mod.ts";
